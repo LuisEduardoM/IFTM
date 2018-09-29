4c8710f2d9243683af5c4ac57ff2896fefa97b62
Anna Clara Pereira <annaclarapereiradovale@gmail.com>
2018-09-29 11:34:55 -0300

    comitando servico rest atividade

--- ArquiteturaOrientadaServico/ExtensaoSTS/src/main/java/br/edu/iftm/extensaoSTS/resources/AtividadeResource.java
+++ ArquiteturaOrientadaServico/ExtensaoSTS/src/main/java/br/edu/iftm/extensaoSTS/resources/AtividadeResource.java
@@ -1,22 +1,28 @@
 package br.edu.iftm.extensaoSTS.resources;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.iftm.extensaoSTS.domain.Atividade;
+import br.edu.iftm.extensaoSTS.repositories.IAtividadeRepository;
 
 @RestController
 @RequestMapping(value = "/atividades")
 public class AtividadeResource {
+	
+	@Autowired
+	private IAtividadeRepository atividadeRepositorio;
 
 	@GetMapping
 	public List<Atividade> listar(){
-		Atividade a1 = new Atividade(1, "visita tecnica gdg");
+		/*Atividade a1 = new Atividade(1, "visita tecnica gdg");
 		Atividade a2 = new Atividade(2, "simpós");
-		return Arrays.asList(a1,a2);
+		return Arrays.asList(a1,a2);*/
+		
+		return atividadeRepositorio.findAll();
 	}
 }
