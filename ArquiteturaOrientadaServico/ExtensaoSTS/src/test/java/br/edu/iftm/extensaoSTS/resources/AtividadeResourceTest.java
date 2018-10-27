2a637afc63ec1dd54bc3754d4b7d7067ed3c1ec7
Izabela Soares <izabelasoares001@gmail.com>
2018-10-27 11:13:04 -0300

    correcao testes

--- ArquiteturaOrientadaServico/ExtensaoSTS/src/test/java/br/edu/iftm/extensaoSTS/resources/AtividadeResourceTest.java
+++ ArquiteturaOrientadaServico/ExtensaoSTS/src/test/java/br/edu/iftm/extensaoSTS/resources/AtividadeResourceTest.java
@@ -1,6 +1,12 @@
 package br.edu.iftm.extensaoSTS.resources;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,22 +19,28 @@ import org.springframework.web.context.WebApplicationContext;
 @SpringBootTest
 public class AtividadeResourceTest {
 
-	@Autowired
+	/*@Autowired
 	private WebApplicationContext context;
 	
 	private MockMvc mvc;
 	
-	private String url = "/atividade/1";
+	private String uri = "/atividade/1";
 
 	@Before
 	private void setup() {
 		this.mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
 	}
 	
-	/*@Test
-	public void testaId() {
-		this.mvc.perform(MockMvcRequestBuilders.get(url))
-		.andExpect(MockMvcResultMatchers.status().isOk())
-		.andExpect(MockMvcResultMatchers.jsonPath("nome", MockMvcResultMatchers.status().equals("Visita Técnica GDG 2018")));
+	@Test
+	public void testaId() throws Exception {
+		this.mvc.perform(get(uri))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("nome", is("Visita Técnica GDG 2018")));
+	}
+	
+	@Test
+	public void testaNotFound() throws Exception {
+		this.mvc.perform(get("/atividade/10"))
+			.andExpect(status().isNotFound());
 	}*/
 }
